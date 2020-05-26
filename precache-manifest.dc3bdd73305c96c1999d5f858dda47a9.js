aabee860bbd3d9527b329680673d441fb2a5b2a7
codeBlu3 <ephinsular@gmail.com>
2020-05-26 16:22:06 +0800

    Updates

---
+++ precache-manifest.dc3bdd73305c96c1999d5f858dda47a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83a7875d42acb62fdfbe03321cf37111",
+    "url": "/staticweb/index.html"
+  },
+  {
+    "revision": "25d7fc42677c560f5a7e",
+    "url": "/staticweb/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2170dedfd2e16e2d6aa1",
+    "url": "/staticweb/static/js/2.b93c07fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/staticweb/static/js/2.b93c07fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25d7fc42677c560f5a7e",
+    "url": "/staticweb/static/js/main.52feeac7.chunk.js"
+  },
+  {
+    "revision": "4bdff1468700eefa3e0f",
+    "url": "/staticweb/static/js/runtime-main.fe4b24b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/staticweb/static/media/logo.5d5d9eef.svg"
+  }
+]);
